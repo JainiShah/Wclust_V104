4543550af1260a405298afa31f5393d790845de4
aomurtag <aomurtag@6a83fd4a-3720-4e4b-8899-71ce5011cf14>
2009-09-28 22:06:30 +0000

    Initial import

---
+++ MyVers.cpp
@@ -0,0 +1,52 @@
+// $Id: MyVers.cpp,v 1.37 2009/01/23 00:56:06 samn Exp $ 
+#include "stdafx.h"
+#include "MyVers.h"
+
+HRESULT LastError () {
+   HRESULT hr = HRESULT_FROM_WIN32(GetLastError());
+   if (SUCCEEDED(hr)) 
+      return E_FAIL;
+   return hr;
+}
+
+// This little function emits an error message based on WIN32 error messages
+void EmitErrorMsg (HRESULT hr) {
+   char szMsg[1024];
+   FormatMessage( 
+      FORMAT_MESSAGE_FROM_SYSTEM, 
+      NULL,
+      hr,
+      MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+      szMsg,
+      1024,
+      NULL 
+      );
+        printf("%s\n",szMsg);
+}
+
+
+// This function gets the file version info structure
+HRESULT GetFileVersion (char *filename, VS_FIXEDFILEINFO *pvsf) {
+   DWORD dwHandle;
+   DWORD cchver = GetFileVersionInfoSize(filename,&dwHandle);
+   if (cchver == 0)
+      return LastError();
+   char* pver = new char[cchver];
+   BOOL bret = GetFileVersionInfo(filename,dwHandle,cchver,pver);
+   if (!bret)
+      return LastError();
+   UINT uLen;
+   void *pbuf;
+   bret = VerQueryValue(pver,"\\",&pbuf,&uLen);
+   if (!bret)
+      return LastError();
+   memcpy(pvsf,pbuf,sizeof(VS_FIXEDFILEINFO));
+   delete[] pver;
+   return S_OK;
+} 
+
+CString GetWClustVersionString()
+{
+	CString strV("2.0.0.104");
+	return strV;
+}
